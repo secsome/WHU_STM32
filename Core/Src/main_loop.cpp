004a83816a8781366b4b91c6315e49e23c3e65ec
secsome <302702960@qq.com>
2024-05-20 23:02:25 +0800

    Update the loop body to check if the input is working correctly

--- Core/Src/main_loop.cpp
+++ Core/Src/main_loop.cpp
@@ -5,11 +5,29 @@
 #include "zlg7290.h"
 #include "lm75a.h"
 
+extern "C" uint8_t KeyPressed;
+
 void MainLoop_Body()
 {
+    HAL_StatusTypeDef status;
+    if (KeyPressed)
     {
-        uint8_t test = 0x0d;
-        ZLG7290_Write(&hi2c1, ZLG7290_ADDR_DPRAM0, &test, 1);
+        KeyPressed = 0;
+
+        uint8_t buffer[3];
+        status = ZLG7290_Read(&hi2c1, ZLG7290_ADDR_KEY, buffer, sizeof(buffer));
+        if (status == HAL_OK)
+        {
+            const uint8_t key = buffer[0];
+            const uint8_t repeat = buffer[1]; UNUSED(repeat);
+            const uint8_t func = buffer[2]; UNUSED(func);
+            // Just show a simple digit 1 on the screen for now
+            if (key != 0)
+            {
+                uint8_t tmp = 0x0C;
+                ZLG7290_Write(&hi2c1, ZLG7290_ADDR_DPRAM0, &tmp, sizeof(tmp));
+            }
+        }
     }
 
     const uint16_t temp = LM75A_GetTemp();
